2e12a68acc20acbd64c8fc66ab6d3f879e801e4d
Dilmohit Singh Bhatha <dillonsingh@hotmail.com>
2025-02-05 15:06:22 +0100

    layout navbar

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\MyController;
+
+Route::get('/', [MyController::class, 'index']);
